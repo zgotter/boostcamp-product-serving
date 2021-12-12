a8b276bf47edce4957f6d637fa39b34c4a575a24
zgotter <shkim4738@gmail.com>
2021-12-12 13:35:52 +0900

    fastapi

---
+++ part3/01_fastapi/examples/02_path_parameter.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/users/{user_id}")
+def read_root(user_id):
+    return {"user_id": user_id}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
